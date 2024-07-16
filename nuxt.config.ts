8007e20e32c4cab21ab45c902370a6015ba39783
Alexandr Protasov <alexprotos1994@gmail.com>
2024-07-16 16:29:07 +0300

    add linters

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+
   app: {
     baseURL: '/nuxt-minimize-market/'
-  }
+  },
+
+  modules: [
+    "@nuxt/eslint",
+    '@nuxtjs/stylelint-module',
+  ]
 })
